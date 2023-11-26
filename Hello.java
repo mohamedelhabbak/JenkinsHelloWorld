5d1cd715c9566a38452b31c809663431d581c590
mohamed <mohamedhabbak99@gmail.com>
2023-11-26 13:23:20 +0200

    Adding new files

---
+++ Hello.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * Hello
+ */
+public class Hello {
+        public static int add(int num1 , int num2){
+            return num1+num2;
+
+        }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Mohamed Welcome to Jenkins For Java");
+        System.out.println("Here is a small Calculator Program");
+        System.out.println("Please enter the first number: ");
+        // Scanner scanner = new Scanner(System.in);
+        // int num1= scanner.nextInt();
+        // System.out.println("Please enter the second number");
+        // int num2=scanner.nextInt();
+
+        // int sum=add(num1,num2);
+        // System.out.println("Here is the Sum of The two entrred numbers: "+sum);
+        
+
+        
+    }
+} 
